846077715051e1f6dcc156f92472343103d05e68
Niklas <niklas.sm+github@gmail.com>
2023-12-15 13:54:01 -0500

    Fix Zenodo ID

--- scripts/fetch_dataset.sh
+++ scripts/fetch_dataset.sh
@@ -6,7 +6,7 @@ data_dir="$script_path/../data"
 mkdir -p $data_dir
 cd $data_dir
 
-dataset_url="https://zenodo.org/records/10291625/files/combination-gesture-dataset.zip?download=1"
+dataset_url="https://zenodo.org/records/10359729/files/combination-gesture-dataset.zip?download=1"
 
 echo "Downloading dataset from: $dataset_url to: $data_dir"
 curl $dataset_url --output combination-gesture-dataset.zip 
